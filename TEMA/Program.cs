c6c5990eddc4a5af59da377eab68133c8e759833
CatalinaDominte <56718210+CatalinaDominte@users.noreply.github.com>
2019-12-06 16:53:04 +0200

    06122019

---
+++ TEMA/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            sqlManager test = new sqlManager();
+
+
+            /*Ex.1 Create a console app named InsertPublisherApp to connect to a local database.
+            2. With this console read the name of the publisher and insert a new publisher to the database.
+            -Use SQL parameters for that
+            -Print the inserted id (Execute scalar with select identity)*/
+            Dictionary<string, SqlParameter> Parameters = new Dictionary<string, SqlParameter>();
+            Parameters["a"] = new SqlParameter("@Id", "21");
+            Parameters["b"] = new SqlParameter("@name", "Librisss");
+            //test.ScalarWithSelectIdentity("INSERT INTO [Publisher] ([PublisherId],[Name]) VALUES(@Id, @name); SELECT [PublisherId] From [Publisher] where [Name]=@name", Parameters);
+
+
+            /* Ex.3 Create a new console app named SummaryPublisherApp. Here print to console:
+            -Number of rows from the Publisher table(Execute scalar)
+            -Top 10 publishers(Id, Name)(SQL Data Reader)
+            -Number of books for each publisher (Publisher Name, Number of Books)
+            -The total price for books for a publisher*/
+
+            //test.Reader("SELECT TOP 10[PublisherId], [Name] FROM[Publisher]", "PublisherId", "Name");
+            //test.Reader("SELECT  [Name], COUNT([BookId]) AS CountB FROM [Publisher] LEFT JOIN [Book] ON [Publisher].[PublisherId] =[Book].[PublisherId] GROUP BY [Name]", "Name", "CountB");
+            // test.Reader("SELECT  [Name], SUM([Price]) AS SumB FROM [Publisher] LEFT JOIN [Book] ON [Publisher].[PublisherId] =[Book].[PublisherId] GROUP BY [Name]", "Name", "SumB");
+            // test.Scalar("SELECT COUNT([PublisherId]) FROM [Publisher]");
+
+            /* Ex.5. Create a console app named CrudBookApp to connect to a local database.
+                 Use SQL parameters for that
+                Print the inserted id (Execute scalar with select identity)*/
+
+            //-----Update a book (read id from console) 
+            Dictionary<string, SqlParameter> Parameters1 = new Dictionary<string, SqlParameter>();
+            Parameters["a"] = new SqlParameter("'@Title'", "thgfr");
+            Parameters["b"] = new SqlParameter("@PublisherId", "2");
+            Parameters["c"] = new SqlParameter("'@Year'", "1999");
+            Parameters["d"] = new SqlParameter("'@Price'", "35");
+
+            //test.ScalarWithSelectIdentity("INSERT INTO [Book] ([Title],[PublisherId],[Year],[Price]) VALUES ('@Title', '@PublisherId', '@Year', '@Price'); SELECT [BookId] FROM[Book] WHERE[Price]= '@Price';", Parameters1);
+            //test.ScalarUpdate();
+
+            //------Delete a book (read id from console)
+            //test.ScalarDelete();
+
+            //------Select a book (read id from console)
+            test.ScalarSelect();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TEMA/sqlManager.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEMA
+{
+    class sqlManager
+    {
+        public SqlConnection Connection()
+        {
+
+            string con = "Data Source=.;Initial Catalog=" + "HomeworkWeek09" + ";Integrated Security=True";
+            SqlConnection cn = new SqlConnection(con);
+            cn.Open();
+            return cn;
+        }
+
+        public void ScalarWithSelectIdentity(string commandQuery, Dictionary<string, SqlParameter> procParameters)
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+                SqlCommand SqlScalarCommand = new SqlCommand(commandQuery, connection);
+
+                foreach (var procParameter in procParameters)
+                {
+                    SqlScalarCommand.Parameters.Add(procParameter.Value);
+                }
+
+
+                var rc = SqlScalarCommand.ExecuteScalar();
+
+                Console.WriteLine($"The Id is : {rc}");
+
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void Scalar(string commandQuery)
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+                SqlCommand SqlScalarCommand = new SqlCommand(commandQuery, connection);
+
+                var command = SqlScalarCommand.ExecuteScalar();
+
+                Console.WriteLine($"The result of the query is : {command}");
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void Reader(string commandQuery, string param1, string param2)
+        {
+            SqlDataReader command = null;
+            SqlConnection connection = Connection();
+            try
+            {
+                SqlCommand dataReader = new SqlCommand(commandQuery, connection);
+
+                command = dataReader.ExecuteReader();
+
+                while (command.Read())
+                {
+                    var currentRow = command;
+                    var x = currentRow[param1];
+                    var y = currentRow[param2];
+
+                    Console.WriteLine($"{x} - {y}");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (command != null)
+                {
+                    command.Close();
+                }
+                connection.Close();
+            }
+        }
+        public void ScalarDelete()
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+
+
+                Console.WriteLine("Enter the name of the Book: ");
+                var name = Console.ReadLine();
+                string commandQuery = "SELECT[BookId] FROM[Book] WHERE[Title] = @title; DELETE FROM [Book] WHERE [Title] = @title;";
+                SqlParameter nameParam1 = new SqlParameter("@title", name);
+                SqlCommand SqlScalarCommand = new SqlCommand(commandQuery, connection);
+                SqlScalarCommand.Parameters.Add(nameParam1);
+                int comand = Convert.ToInt32(SqlScalarCommand.ExecuteScalar());
+                Console.WriteLine($"The Id is : {comand}");
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public void ScalarUpdate()
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+                Console.WriteLine("Enter the name of the Book: ");
+                var name =Console.ReadLine();
+                Console.WriteLine("Enter the new price: ");
+                var price = int.Parse(Console.ReadLine());
+
+              
+                string commandQuery = "UPDATE [Book] SET [Price] = @price WHERE [Title] =@name; SELECT [BookId] FROM [Book] WHERE [Price] = @price";
+                SqlParameter nameParam2 = new SqlParameter("@name", name);
+                SqlParameter nameParam1 = new SqlParameter("@price", price);
+                
+
+                SqlCommand SqlScalarCommand = new SqlCommand(commandQuery, connection);
+                SqlScalarCommand.Parameters.Add(nameParam1);
+                SqlScalarCommand.Parameters.Add(nameParam2);
+
+                var comand = SqlScalarCommand.ExecuteScalar();
+                Console.WriteLine($"The Id is : {comand}");
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void ScalarSelect()
+        {
+            try
+            {
+                SqlConnection connection = Connection();
+                Console.WriteLine("Enter the name of the Book: ");
+                string name = Console.ReadLine();
+
+                string commandQuery = "SELECT [BookId] FROM [Book] WHERE [Title] = @name";
+                SqlParameter nameParam = new SqlParameter("@name", name);
+                SqlCommand SqlScalarCommand = new SqlCommand(commandQuery, connection);
+                SqlScalarCommand.Parameters.Add(nameParam);
+
+                var comand = SqlScalarCommand.ExecuteScalar();
+                Console.WriteLine($"The Id is : {comand}");
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
